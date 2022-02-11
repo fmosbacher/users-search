7463f01beff7c9125245e3682cb9012975e46c93
Fernando Azevedo <azevedo.fma@gmail.com>
2022-02-11 12:19:15 -0300

    Create github service

---
+++ src/services/github.ts
@@ -0,0 +1,21 @@
+import { User } from '../types';
+
+const queryUser = async (user: string): Promise<User[]> => {
+  const res = await fetch(
+    `https://api.github.com/search/users?q=${user}+in:user`,
+    {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    }
+  );
+  const data = await res.json();
+
+  return data.items.map((item: { login: string; id: number }) => ({
+    name: item.login,
+    id: item.id,
+  }));
+};
+
+export { queryUser };
