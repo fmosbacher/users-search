d884d2080cd87e8931a0dc06812e1ff8df042f3e
Fernando Azevedo <azevedo.fma@gmail.com>
2022-02-11 12:02:37 -0300

    Configure application structure

---
+++ webpack.config.ts
@@ -0,0 +1,52 @@
+import path from 'path';
+import * as webpack from 'webpack';
+import * as webpackDevServer from 'webpack-dev-server';
+
+const config: webpack.Configuration = {
+  entry: './src/index.tsx',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript',
+            ],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    static: path.join(__dirname, 'build'),
+    compress: true,
+    port: 3000,
+  },
+};
+
+export default config;
